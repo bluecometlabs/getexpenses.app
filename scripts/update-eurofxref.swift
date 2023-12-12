a64afcca2d78cd2b07e4546c49d3dc9f371f5eb1
Hiroki Nagasawa <2482478+pixyzehn@users.noreply.github.com>
2023-12-12 21:46:22 +0900

    Update swift script

--- scripts/update-eurofxref.swift
+++ scripts/update-eurofxref.swift
@@ -8,11 +8,172 @@ struct ReferenceRatesRef: Decodable {
     var rates: [String: Double]
 }
 
-let base = "EUR"
-let symbols = "AED,AFN,ALL,AMD,ANG,AOA,ARS,AUD,AWG,AZN,BAM,BBD,BDT,BGN,BHD,BIF,BMD,BND,BOB,BRL,BSD,BTN,BWP,BYN,BYR,BZD,CAD,CDF,CHF,CLF,CLP,CNY,COP,CRC,CUC,CUP,CVE,CZK,DJF,DKK,DOP,DZD,EGP,ERN,ETB,FJD,FKP,GBP,GEL,GHS,GIP,GMD,GNF,GTQ,GYD,HKD,HNL,HRK,HTG,HUF,IDR,ILS,INR,IQD,IRR,ISK,JMD,JOD,JPY,KES,KGS,KHR,KMF,KPW,KRW,KWD,KYD,KZT,LAK,LBP,LKR,LRD,LSL,LTL,LVL,LYD,MAD,MDL,MGA,MKD,MMK,MNT,MOP,MUR,MVR,MWK,MXN,MYR,MZN,NAD,NGN,NIO,NOK,NPR,NZD,OMR,PAB,PEN,PGK,PHP,PKR,PLN,PYG,QAR,RON,RSD,RUB,RWF,SAR,SBD,SCR,SDG,SEK,SGD,SHP,SLL,SOS,SRD,STD,SYP,SZL,THB,TJS,TMT,TND,TOP,TRY,TTD,TWD,TZS,UAH,UGX,USD,UYU,UZS,VND,VUV,WST,XAF,XAG,XAU,XCD,XDR,XOF,XPF,YER,ZAR,ZMK,ZMW,ZWL"
-let expectedNumberOfCurrencyCodes = symbols.components(separatedBy: ",").count
+let symbols = [
+    "AED", // United Arab Emirates Dirham
+    "AFN", // Afghan Afghani
+    "ALL", // Albanian Lek
+    "AMD", // Armenian Dram
+    "ANG", // Netherlands Antillean Guilder
+    "AOA", // Angolan Kwanza
+    "ARS", // Argentine Peso
+    "AUD", // Australian Dollar
+    "AWG", // Aruban Florin
+    "AZN", // Azerbaijani Manat
+    "BAM", // Bosnia-Herzegovina Convertible Mark
+    "BBD", // Barbadian Dollar
+    "BDT", // Bangladeshi Taka
+    "BGN", // Bulgarian Lev
+    "BHD", // Bahraini Dinar
+    "BIF", // Burundian Franc
+    "BMD", // Bermudian Dollar
+    "BND", // Brunei Dollar
+    "BOB", // Bolivian Boliviano
+    "BRL", // Brazilian Real
+    "BSD", // Bahamian Dollar
+    "BTN", // Bhutanese Ngultrum
+    "BWP", // Botswanan Pula
+    "BYN", // Belarusian Ruble
+    "BYR", // Belarusian Ruble (old code)
+    "BZD", // Belize Dollar
+    "CAD", // Canadian Dollar
+    "CDF", // Congolese Franc
+    "CHF", // Swiss Franc
+    "CLF", // Chilean Unit of Account (UF)
+    "CLP", // Chilean Peso
+    "CNY", // Chinese Yuan
+    "COP", // Colombian Peso
+    "CRC", // Costa Rican Colón
+    "CUC", // Cuban Convertible Peso
+    "CUP", // Cuban Peso
+    "CVE", // Cape Verdean Escudo
+    "CZK", // Czech Republic Koruna
+    "DJF", // Djiboutian Franc
+    "DKK", // Danish Krone
+    "DOP", // Dominican Peso
+    "DZD", // Algerian Dinar
+    "EGP", // Egyptian Pound
+    "ERN", // Eritrean Nakfa
+    "ETB", // Ethiopian Birr
+    "FJD", // Fijian Dollar
+    "FKP", // Falkland Islands Pound
+    "GBP", // British Pound Sterling
+    "GEL", // Georgian Lari
+    "GHS", // Ghanaian Cedi
+    "GIP", // Gibraltar Pound
+    "GMD", // Gambian Dalasi
+    "GNF", // Guinean Franc
+    "GTQ", // Guatemalan Quetzal
+    "GYD", // Guyanaese Dollar
+    "HKD", // Hong Kong Dollar
+    "HNL", // Honduran Lempira
+    "HRK", // Croatian Kuna
+    "HTG", // Haitian Gourde
+    "HUF", // Hungarian Forint
+    "IDR", // Indonesian Rupiah
+    "ILS", // Israeli New Shekel
+    "INR", // Indian Rupee
+    "IQD", // Iraqi Dinar
+    "IRR", // Iranian Rial
+    "ISK", // Icelandic Króna
+    "JMD", // Jamaican Dollar
+    "JOD", // Jordanian Dinar
+    "JPY", // Japanese Yen
+    "KES", // Kenyan Shilling
+    "KGS", // Kyrgystani Som
+    "KHR", // Cambodian Riel
+    "KMF", // Comorian Franc
+    "KPW", // North Korean Won
+    "KRW", // South Korean Won
+    "KWD", // Kuwaiti Dinar
+    "KYD", // Cayman Islands Dollar
+    "KZT", // Kazakhstani Tenge
+    "LAK", // Laotian Kip
+    "LBP", // Lebanese Pound
+    "LKR", // Sri Lankan Rupee
+    "LRD", // Liberian Dollar
+    "LSL", // Lesotho Loti
+    "LTL", // Lithuanian Litas
+    "LVL", // Latvian Lats
+    "LYD", // Libyan Dinar
+    "MAD", // Moroccan Dirham
+    "MDL", // Moldovan Leu
+    "MGA", // Malagasy Ariary
+    "MKD", // Macedonian Denar
+    "MMK", // Myanma Kyat
+    "MNT", // Mongolian Tugrik
+    "MOP", // Macanese Pataca
+    "MUR", // Mauritian Rupee
+    "MVR", // Maldivian Rufiyaa
+    "MWK", // Malawian Kwacha
+    "MXN", // Mexican Peso
+    "MYR", // Malaysian Ringgit
+    "MZN", // Mozambican Metical
+    "NAD", // Namibian Dollar
+    "NGN", // Nigerian Naira
+    "NIO", // Nicaraguan Córdoba
+    "NOK", // Norwegian Krone
+    "NPR", // Nepalese Rupee
+    "NZD", // New Zealand Dollar
+    "OMR", // Omani Rial
+    "PAB", // Panamanian Balboa
+    "PEN", // Peruvian Nuevo Sol
+    "PGK", // Papua New Guinean Kina
+    "PHP", // Philippine Peso
+    "PKR", // Pakistani Rupee
+    "PLN", // Polish Złoty
+    "PYG", // Paraguayan Guarani
+    "QAR", // Qatari Rial
+    "RON", // Romanian Leu
+    "RSD", // Serbian Dinar
+    "RUB", // Russian Ruble
+    "RWF", // Rwandan Franc
+    "SAR", // Saudi Riyal
+    "SBD", // Solomon Islands Dollar
+    "SCR", // Seychellois Rupee
+    "SDG", // Sudanese Pound
+    "SEK", // Swedish Krona
+    "SGD", // Singapore Dollar
+    "SHP", // Saint Helena Pound
+    "SLL", // Sierra Leonean Leone
+    "SOS", // Somali Shilling
+    "SRD", // Surinamese Dollar
+    "STD", // São Tomé and Príncipe Dobra
+    "SYP", // Syrian Pound
+    "SZL", // Swazi Lilangeni
+    "THB", // Thai Baht
+    "TJS", // Tajikistani Somoni
+    "TMT", // Turkmenistani Manat
+    "TND", // Tunisian Dinar
+    "TOP", // Tongan Paʻanga
+    "TRY", // Turkish Lira
+    "TTD", // Trinidad and Tobago Dollar
+    "TWD", // New Taiwan Dollar
+    "TZS", // Tanzanian Shilling
+    "UAH", // Ukrainian Hryvnia
+    "UGX", // Ugandan Shilling
+    "USD", // United States Dollar
+    "UYU", // Uruguayan Peso
+    "UZS", // Uzbekistan Som
+    "VND", // Vietnamese Đồng
+    "VUV", // Vanuatu Vatu
+    "WST", // Samoan Tala
+    "XAF", // Central African CFA Franc
+    "XAG", // Silver Ounce
+    "XAU", // Gold Ounce
+    "XCD", // East Caribbean Dollar
+    "XDR", // Special Drawing Rights
+    "XOF", // West African CFA Franc
+    "XPF", // CFP Franc
+    "YER", // Yemeni Rial
+    "ZAR", // South African Rand
+    "ZMK", // Zambian Kwacha (pre-2013)
+    "ZMW", // Zambian Kwacha
+    "ZWL"  // Zimbabwean Dollar
+].sorted()
 
-let url = "https://api.apilayer.com/fixer/latest?base=\(base)&symbols=\(symbols)"
+let base = "EUR"
+let expectedNumberOfCurrencyCodes = symbols.count
+let url = "https://api.apilayer.com/fixer/latest?base=\(base)&symbols=\(symbols.joined(separator: ","))"
 let apiKey = CommandLine.arguments[1]
 
 var request = URLRequest(url: URL(string: url)!,timeoutInterval: Double.infinity)
@@ -36,7 +197,7 @@ let task = URLSession.shared.dataTask(with: request) { data, response, error in
 
         if numberOfCurrencyCodes != expectedNumberOfCurrencyCodes {
             let currencyCodes = referenceRatesRef.rates.map { $0.0 }.sorted()
-            let expectedCurrencyCodes = symbols.components(separatedBy: ",").sorted()
+            let expectedCurrencyCodes = symbols
             let diff = currencyCodes.difference(from: expectedCurrencyCodes)
             fatalError("Error: number of currency codes does not match [\(diff.joined(separator: ", "))]")
         }
